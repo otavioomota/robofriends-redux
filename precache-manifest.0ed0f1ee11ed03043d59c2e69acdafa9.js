0859164a5cf53d48e3975f7393f877da2527cece
otavioomota <41590014+otavioomota@users.noreply.github.com>
2018-10-20 16:57:37 -0300

    Updates

---
+++ precache-manifest.0ed0f1ee11ed03043d59c2e69acdafa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e0b58d846368f426a94",
+    "url": "/robofriends-redux/static/js/runtime~main.9e0b58d8.js"
+  },
+  {
+    "revision": "c0e5974e49944dc1f9a2",
+    "url": "/robofriends-redux/static/js/main.c0e5974e.chunk.js"
+  },
+  {
+    "revision": "aaaa65ebb118d0e2b469",
+    "url": "/robofriends-redux/static/js/1.aaaa65eb.chunk.js"
+  },
+  {
+    "revision": "c0e5974e49944dc1f9a2",
+    "url": "/robofriends-redux/static/css/main.4916ce47.chunk.css"
+  },
+  {
+    "revision": "aaaa65ebb118d0e2b469",
+    "url": "/robofriends-redux/static/css/1.4b35c1a5.chunk.css"
+  },
+  {
+    "revision": "ffd3885e9fba9f790f6d74c5a4783c7d",
+    "url": "/robofriends-redux/index.html"
+  }
+];
